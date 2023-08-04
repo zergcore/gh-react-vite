65824a6653c0cdaaf198cb8e81a1328e9e5d22ba
Zaidibeth Ramos <zaidi.ramos@gmail.com>
2023-08-03 22:26:02 -0400

    fix base url bug

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://zergcore.github.io/gh-react-vite/',
   plugins: [react()],
 })
